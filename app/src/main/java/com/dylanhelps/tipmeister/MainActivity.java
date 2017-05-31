cdc51c604ff94de55afffc973eca2ed68b1498e5
Dylan Chen <hchennyc@outlook.com>
2017-05-31 15:10:37 -0400

    Init

---
+++ app/src/main/java/com/dylanhelps/tipmeister/MainActivity.java
@@ -0,0 +1,195 @@
+package com.dylanhelps.tipmeister;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SurfaceView cameraView;
+    private TextView textView;
+    private CameraSource cameraSource;
+    private final int RequestCameraPermissionID = 1001;
+    private String TAG = this.getClass().getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        cameraView = (SurfaceView) findViewById(R.id.surface_view);
+        textView =(TextView) findViewById(R.id.text_view);
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        if (!textRecognizer.isOperational()) {
+            Log.w("MainActivity", "Detector dependencies are not yet available");
+        } else {
+            setUpCameraSource(textRecognizer);
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode==RequestCameraPermissionID){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                if (!checkLocationPermission())
+                    Log.e(TAG,"Camera permission denied.");
+                    return;
+                }
+
+                try {
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException e) {
+                    Log.e(TAG,e.getMessage());
+                }
+            }
+        }
+
+
+    private boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // show explanation or not
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.CAMERA)){
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},RequestCameraPermissionID);
+            }
+            else {
+                // no explanation needed. Request permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA}, RequestCameraPermissionID);
+            }
+            return false;
+        }
+        else return true;
+    }
+
+    private void setUpCameraSource(TextRecognizer textRecognizer){
+        // initialize camera source
+        cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedPreviewSize(1280, 1024)
+                .setRequestedFps(2.0f)
+                .setAutoFocusEnabled(true)
+                .build();
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                try {
+                    checkLocationPermission();
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException e) {
+                    Log.e(TAG,"Cannot initialize camera source");
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        }); // end addCallback
+    }// end setUpCameraSource
+
+
+    private void setUpTextRec(TextRecognizer textRecognizer){
+        textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<TextBlock> detections) {
+
+                final SparseArray<TextBlock> items = detections.getDetectedItems();
+                if(items.size() != 0)
+                {
+                    String str;
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            for(int i =0;i<items.size();++i)
+                            {
+                                TextBlock item = items.valueAt(i);
+                                stringBuilder.append(item.getValue());
+                                stringBuilder.append("\n");
+
+                            }
+                            textView.setText(stringBuilder.toString());
+                        }
+                    }); // end new runnable
+                }
+            }
+        });// end setProcessor
+
+    }
+}
